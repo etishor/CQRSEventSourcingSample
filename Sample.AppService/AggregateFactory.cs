e54d57888f3d6c7a44dd12e37c59dcc156a80311
Iulian Margarintescu <etishor@gmail.com>
2011-03-12 11:23:46 +0200

    Add comment

--- Sample.AppService/AggregateFactory.cs
+++ Sample.AppService/AggregateFactory.cs
@@ -8,13 +8,19 @@ using System.Reflection;
 
 namespace Sample.AppService
 {
+    /// <summary>
+    /// Factory for creating aggregates from with their Id using a private constructor that accespts
+    /// only one paramenter, the id of the aggregate to create.
+    /// This factory is used by the event store to create an aggregate prior to replaying it's events.
+    /// </summary>
     public class AggregateFactory : IConstructAggregates
     {
         public IAggregate Build(Type type, Guid id, IMemento snapshot)
         {
-            ConstructorInfo constructor = type.GetConstructor(BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(Guid) }, null);
+            ConstructorInfo constructor = type.GetConstructor(
+                BindingFlags.NonPublic | BindingFlags.Instance, null, new Type[] { typeof(Guid) }, null);
+
             return constructor.Invoke(new object[] { id }) as IAggregate;
         }
-
     }
 }
